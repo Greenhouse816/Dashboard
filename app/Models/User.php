91d7b0ae61854b32ed20e46182a1d4b015c57dc3
greenhouse <greenhouse8161@gmail.com>
2024-08-26 18:20:53 +0900

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -14,7 +14,7 @@ class User extends Authenticatable
   /**
    * The attributes that are mass assignable.
    *
-   * @var array<int, string>
+   * @var array<int, string>             
    */
   protected $fillable = [
     'name',
